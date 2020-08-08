0ec4381a58e1f00e18cb1750e05ad571b8bbe749
ObaidAshraf <ubaidashraf91@gmail.com>
2020-08-08 22:57:45 +0500

    First Commit. Also added various rootQuery model and mongoDB connection to mLab. Also created mongoDB Schemas

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
+const schema = require('./schema/schema')
+const mongoose = require('mongoose')
+
+const app = express()
+
+mongoose.connect('mongodb://admin:admin123@ds129939.mlab.com:29939/gql-library')
+mongoose.connection.once('open', () => {
+    console.log("Connected to database.")
+})
+
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    graphiql: true
+}))
+
+app.listen(4000, () => {
+    console.log("Now listening for requests on port 4000")
+})
